9157b8615d56d1130f7501a0d50f0899eb48d1a9
MaksGorobets <gorobets542@gmail.com>
2023-11-08 15:13:44 +0200

    Initial Commit

---
+++ AnimationsDay2/AnimationsDay2App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsDay2App.swift
+//  AnimationsDay2
+//
+//  Created by Maks Winters on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsDay2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
